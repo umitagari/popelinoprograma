ea7cf0409d9d55dd20bfe7aa1d78d1e0ef0dc964
cold dreams <74847995+notstzxdev@users.noreply.github.com>
2022-03-02 19:25:23 -0300

    Add files via upload

---
+++ popelino/popelino.py
@@ -0,0 +1,13 @@
+import sys
+
+es = "es popelino"
+noes = "no es popelino!"
+
+if True:
+    gato=input()
+
+    if gato == 'popelino':
+        print(es)
+        sys.exit()
+    else:
+        print(noes)
